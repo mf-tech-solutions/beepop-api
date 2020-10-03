287430ff8efe60baab154d5d66276dd6ff6af30f
Ikaro Souza <icaruswebdev@gmail.com>
2020-10-03 17:09:14 -0300

    updated typeorm configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,12 +7,20 @@ import { IntegrationModule } from "./modules/integration/integration.module";
 
 @Module({
   imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRoot({
+      type: "sqlite",
+      database: "./db.sqlite3",
+      autoLoadEntities: true,
+      migrations: ["dist/**/*migrations/*.js"],
+      cli: {
+        migrationsDir: "src/migrations",
+      },
+    }),
+
     CommonModule,
     MusicModule,
     IntegrationModule,
-
-    ConfigModule.forRoot({ isGlobal: true }),
-    TypeOrmModule.forRoot(),
   ],
 })
 export class AppModule {}
